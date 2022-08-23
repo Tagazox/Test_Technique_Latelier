12066e89d800a5039b8a1823c0176bdd4d49be0d
nioch <nioche.hippolyte@gmail.com>
2022-08-24 01:41:40 +0200

    Minor correction, keeping the reference is useless.

--- NetCoreTest.Data.Model/PlayerStatistics.cs
+++ NetCoreTest.Data.Model/PlayerStatistics.cs
@@ -4,7 +4,6 @@ namespace NetCoreTests.Data.Model
 {
     public class PlayersStatistics
     {
-        IEnumerable<Player> _players { get; set; }
         public Country CountryWitchHasTheBestRatio { get; internal set; }
         public int MeanBmiOfThePlayers { get; internal set; }
         public double MedianHeightOfThePlayers { get; internal set; }
@@ -12,18 +11,17 @@ namespace NetCoreTests.Data.Model
         {
             try
             {
-                _players = players;
-                List<Country> countryList = _players.Select(c => c.country).DistinctBy(c => c.code).ToList();
-                int _playersCount = _players.Count();
-                MedianHeightOfThePlayers = _playersCount % 2 == 0
-                    ? _players.Select(x => x.data.height).OrderBy(x => x).Skip((_playersCount / 2) - 1).Take(2).Average()
-                    : _players.Select(x => x.data.height).OrderBy(x => x).ElementAt(_playersCount / 2);
+                List<Country> countryList = players.Select(c => c.country).DistinctBy(c => c.code).ToList();
+                int playersCount = players.Count();
+                MedianHeightOfThePlayers = playersCount % 2 == 0
+                    ? players.Select(x => x.data.height).OrderBy(x => x).Skip((playersCount / 2) - 1).Take(2).Average()
+                    : players.Select(x => x.data.height).OrderBy(x => x).ElementAt(playersCount / 2);
 
                 MeanBmiOfThePlayers = (int)Math.Round(
-                        _players.Select(d => (d.data.weight / 1000) / Math.Pow(d.data.height / 100, 2)).Average()
+                        players.Select(d => (d.data.weight / 1000) / Math.Pow(d.data.height / 100, 2)).Average()
                         , MidpointRounding.AwayFromZero);
 
-                CountryWitchHasTheBestRatio = countryList.First(c => c.code == _players.GroupBy(p => p.country.code)
+                CountryWitchHasTheBestRatio = countryList.First(c => c.code == players.GroupBy(p => p.country.code)
                       .Select(d => new { Score =((float) d.Sum(s => s.data.last.Sum()) / (float)d.Sum(s => s.data.last.Count())), Country = d.Key })
                       .OrderByDescending(d => d.Score)
                       .Select(c => c.Country).First());
